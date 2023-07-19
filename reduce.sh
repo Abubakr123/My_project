ee62b910a2f13a8e6b56288acf477436f15aa861
Abubakr123 <abubakryagob@gmail.com>
2023-07-19 17:42:26 +0200

    removing the files reduce.sh reduceSingleStation.py cleanTot.py

--- reduce.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-files="B0138+59_D20150301T150201 B0138+59_D20150308T133201 B0138+59_D20150315T133201 B0138+59_D20150405T112201 B0138+59_D20150412T112201 B0138+59_D20150419T101701"
-
-for i in $files; do
-	echo $i "in the prosessing"
-	python2.7 reduceSingleStation_modefy.py --stem $i 
-
-done 
